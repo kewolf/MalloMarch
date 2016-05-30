cec5f1843cffbb88184c4d35cc579a8b54047844
brownerthanu <reid.oda@gmail.com>
2016-05-30 13:17:06 -0400

    Added Sync library to reid_playground

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,10 @@ router.get('/', function (req, res, next) {
     res.render('version1', {title: 'Version 1'});
 });
 
+router.get('/reid', function (req, res, next) {
+    res.render('reid_playground', {title: 'playtime'});
+});
+
 ///* GET home page. */
 //router.get('/version1', function(req, res, next) {
 //  res.render('version1', { title: 'Version 1' });
