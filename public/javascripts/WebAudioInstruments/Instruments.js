6794f083dd2c5ee9386564ea3482ab714c8b0919
brownerthanu <reid.oda@gmail.com>
2016-06-06 14:00:25 -0400

    almost ready for realtime control of instruments

--- public/javascripts/WebAudioInstruments/Instruments.js
+++ public/javascripts/WebAudioInstruments/Instruments.js
@@ -1,6 +1,6 @@
-var Drum = function(buffer)
+var Drum = function (buffers)
 {
-    this.buffer = buffer;
+    this.buffers = buffers;
     this.n_drummers = 1;
     this.size = 0;
     this.decay = 5;
@@ -8,11 +8,29 @@ var Drum = function(buffer)
     this.dynamics = 0;
 };
 
-Drum.prototype.play = function(time)
+Drum.prototype.play = function (time, drumIndex)
 {
     this.source = audioContext.createBufferSource();
-    this.source.buffer = this.buffer;
+    this.source.buffer = this.buffers[drumIndex];
     this.source.connect(audioContext.destination);
     this.source.detune.value = (this.pitch - 2000);
     this.source.start(AudioContext.currentTime);
 };
+
+// function loadSnareSound() {
+//     var localBuffer;
+//     var url = 'http://localhost:3000/audio/snare0.wav';
+//     var request = new XMLHttpRequest();
+//     request.open('GET', url, true);
+//     request.responseType = 'arraybuffer';
+//
+//     request.onload = function() {
+//         audioContext.decodeAudioData(request.response, function(buffer) {
+//             localBuffer = buffer;
+//         }, function(e) {
+//             console.log("Error in decoding audio data: " + e.err);
+//         });
+//     };
+//     request.send();
+//     return localBuffer;
+// }
