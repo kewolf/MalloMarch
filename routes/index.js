cca8264aa250480a5c785d98062cec1fbceffeb1
Katie Wolf <wolfx265@gmail.com>
2016-06-08 17:43:18 -0400

    Updated Sliders to change the instruments values.

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,11 @@ router.get('/reid', function (req, res, next) {
     res.render('reid_playground', {title: 'playtime'});
 });
 
+router.get('/katie', function (req, res, next) {
+    res.render('katie_playground', {title: 'playtime'});
+});
+
+
 ///* GET home page. */
 //router.get('/version1', function(req, res, next) {
 //  res.render('version1', { title: 'Version 1' });
