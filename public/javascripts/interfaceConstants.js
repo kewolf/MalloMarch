cd9087a5cdac5aacfb043a21766993478a3c3d33
Katie Wolf <wolfx265@gmail.com>
2016-07-02 14:07:18 +1000

    Added Volume Slider (but didn't implement them), Added code to turn sequencer on/off, created an intermission page, fixed so page opens up to channel 0, added some better presets (still working on creating the final ones).

--- public/javascripts/interfaceConstants.js
+++ public/javascripts/interfaceConstants.js
@@ -3,7 +3,7 @@
  */
 
 //Constants to be used accross both node.js and the clients:
-var InterfaceEnum = Object.freeze({"INFO": 0, "M1": 1, "M2": 2, "SURVEY": 3});
+var InterfaceEnum = Object.freeze({"INFO": 0, "M1": 1, "TUTORIAL": 2, "M2": 3, "SURVEY": 4});
 
 if (typeof window === 'undefined') {
     exports.InterfaceEnum = InterfaceEnum;
