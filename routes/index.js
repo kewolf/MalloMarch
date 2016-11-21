7e1b360e2d8ea63b21518aaa2716d17648350732
Katie Wolf <wolfx265@gmail.com>
2016-11-21 16:21:03 -0500

    Created a way to load in and view/listen to the playback of a previous performances log files.

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,11 @@ router.get('/', function (req, res, next) {
     res.render('version1', {title: 'MalLo March'});
 });
 
+router.get('/replay', function (req, res, next) {
+    res.render('version2', {title: 'MalLo March Replay'});
+});
+
+
 router.get('/reid', function (req, res, next) {
     res.render('reid_playground', {title: 'playtime'});
 });
@@ -17,6 +22,10 @@ router.get('/katie', function (req, res, next) {
 
 router.get('/serverMasterControl', function (req, res, next) {
     res.render('server', {title: 'Server'});
+});
+
+router.get('/playbackControl', function (req, res, next) {
+    res.render('playback', {title: 'Playback Controller'});
 });
 
 ///* GET home page. */
