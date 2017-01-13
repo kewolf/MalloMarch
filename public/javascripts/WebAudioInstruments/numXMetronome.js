90ad3a9eb6498ecd3ca8cbb52015907366cb837a
brownerthanu <reid.oda@gmail.com>
2017-01-13 10:55:31 -0500

    added provision for tempo change

--- public/javascripts/WebAudioInstruments/numXMetronome.js
+++ public/javascripts/WebAudioInstruments/numXMetronome.js
@@ -73,21 +73,18 @@ Metronome = function (audioContext, logger) {
         {
             this.changeTickNum = this.numTicksSoFar;
         }
-
         this.numTicksSoFar = Math.floor(this.syncClient.getTime() / this.tickPeriod);
-
         if (this.tempo == 110) {
-            this.numReportedTicksSoFar = Math.floor(this.syncClient.getTime() / this.tickPeriod);
+            this.numTicksSoFar = Math.floor(this.syncClient.getTime() / this.tickPeriod);
         } else if (this.tempo == 55)
         {
             var curTick = Math.floor(this.syncClient.getTime() / this.tickPeriod);
             var newTicks = curTick - this.changeTickNum / 2;
-            this.numReportedTicksSoFar = this.changeTickNum + newTicks;
+            this.numTicksSoFar = this.changeTickNum + newTicks;
         }
-
         var oldMeasureNum = this.curMeasureNum;
         var oldBeatNum = this.curBeatNum;
-        this.curMeasureNum = Math.floor(this.numReportedTicksSoFar / this.numBeatsInMeasure) + 1;
+        this.curMeasureNum = Math.floor(this.numTicksSoFar / this.numBeatsInMeasure) + 1;
         this.curBeatNum = this.numTicksSoFar % this.numBeatsInMeasure + 1;
         var nextTickTime = (this.numTicksSoFar + 1) * this.tickPeriod;
         if (nextTickTime - this.syncClient.getTime() < this.lookahead &&
